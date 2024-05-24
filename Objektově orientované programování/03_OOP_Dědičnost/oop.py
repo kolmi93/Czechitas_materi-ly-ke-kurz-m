05c89287127da937e732876f9425c61beaaf8328
Michaela Kolarova <kolarova.mi@gmail.com>
2024-05-24 10:11:57 +0200

    course update

--- Objektově orientované programování/03_OOP_Dědičnost/oop.py
+++ Objektově orientované programování/03_OOP_Dědičnost/oop.py
@@ -1,153 +1,156 @@
-class Package:
-    def __init__(self, address, weight, state):
-        self.address = address
-        self.weight = weight
-        self.state = state
+ODDELOVAC = (1*'\n' + 30* '_' + 1*'\n')
+print(ODDELOVAC)
+
+# ÚKOL Č.1 - ZVÍŘATA:
+
+class Zvire:
+    def __init__(self, jmeno: str, druh: str, vaha: int):
+        self.jmeno = jmeno
+        self.druh = druh
+        self.vaha = vaha
 
     def __str__(self):
-        return f"Adresa: {self.address} ({self.weight} Kg) - {self.state}"
+        return f'{self.jmeno} patří mezi druh {self.druh} a váží {self.vaha}.'
+    
+    def export_to_dict(self):
+        jmeno = self.jmeno
+        druh = self.druh
+        vaha = self.vaha
+        dict_zvirata = {
+            'jmeno':jmeno,
+            'druh': druh,
+            'vaha': vaha,
+        }
+        return dict_zvirata
 
-    def delivery_price(self):
-        if self.weight < 10:
-            cena = 129
-        elif self.weight < 20:
-            cena = 159
-        else:
-            cena = 359
-        return cena
+panda = Zvire('Růženka', 'Panda Velká', 150)
+vydra = Zvire('Vilda', 'Vydra Mořská', 20)
+tygr = Zvire('Matýsek', 'Tygr Sumatera', 300)
+medved = Zvire('Karlík', 'Lední Medvěd', 700)
 
-    def deliver(self):
-        if self.state == 'nedoruceno':
-            self.state = 'doruceno'
-            return 'Balik dorucen.'
-        else:
-            return f'Balik na adresu {self.address} jiz byl dorucen.'
+zvirata = [panda, vydra, medved, tygr]
 
-class ValuablePackage(Package):
-    def __init__(self, address, weight, state, value):
-        super().__init__(address, weight, state)
-        self.value = value
+zvirata_dict = []
+
+for zvire in zvirata:
+    zvirata_dict.append(zvire.export_to_dict())
+    print(zvire)
+
+# vypíše slovník zvířat
+print(zvirata_dict)
+
+assert hasattr(zvire, 'jmeno')
+assert hasattr(zvire, 'druh')
+assert hasattr(zvire, 'vaha')
+assert isinstance(zvire.jmeno, str)
+assert isinstance(zvire.druh, str)
+assert isinstance(zvire.vaha, int)
+
+print(ODDELOVAC)
+
+# ÚKOL Č.2 - ZAMĚSTNANEC:
+
+class Zamestnanec:
+    def __init__(self, jmeno: str, rocni_plat: int, pozice: str):
+        self.jmeno = jmeno
+        self.rocni_plat = rocni_plat
+        self.pozice = pozice
 
     def __str__(self):
-        return super().__str__() + f" Cena = {self.value}"
+        return f'{self.jmeno} je na pozici {self.pozice} a roční plat má {self.rocni_plat},- Kč.'
 
-#  1. Celková hodnota balíků
-# Pokračuj ve své práci pro zásilkovou společnost. Společnost chce doplnit do aplikace funkci
-# pro výpočet celkového hodnoty nákladu nějakého auta, aby pak (např. v případě nehody nebo krádeže)
-# mohla snadno spočítat celkovou hodnotu cenných balíků v autě a předat informaci pojišťovně.
-# Příklad je podobný bonusu na výpočet celkové hmotnosti z předchozí části, liší se ale v tom,
-# že hodnotu mají pouze cenné balíky, zatímco hmotnost mají všechny balíky.
+    def ziskej_inicialy(self):
+        jmeno = self.jmeno.split()
+        return f'{jmeno[0][0].upper()}.{jmeno[1][0].upper()}.'
+    
+    def dictionary(self):
+        jmeno = self.jmeno
+        plat = self.rocni_plat
+        pozice = self.pozice
+        dict_zamestnanci  = {
+            'jmeno': jmeno,
+            'plat': plat,
+            'pozice': pozice,
+        }
+        return dict_zamestnanci
+    
+tereza = Zamestnanec('Tereza Vysoká', 700000, 'Cvičitelka tygrů')
+aneta = Zamestnanec('Anet Krasna', 600000, 'Cvičitelka vyder')
+martin = Zamestnanec('Martin Veliký', 650000, 'Cvičitel ledních medvědů')
 
-# Níže je příklad balíků, které můžeš použít pro tvorbu svého programu.
+zamestnanci = [tereza, aneta, martin]
 
-package_1 = ValuablePackage("Grimmauldovo náměstí 11", 1.9, "nedoručen", 5500)
-package_2 = Package("Godrikův důl 47", 1.9, "nedoručen")
-package_3 = ValuablePackage("Vydrník svatého Drába 13", 1.9, "nedoručen", 5500)
-package_list = [package_1, package_2, package_3]
+zamestnanci_list = []
+for zamestnanec in zamestnanci:
+    zamestnanci_list.append(zamestnanec.dictionary())
+    print(zamestnanec.ziskej_inicialy())
 
-total_value = 0
+print(zamestnanci_list)
 
-for one in package_list:
-    if hasattr(one, 'value'):
-        total_value += one.value
-        
-print(f'Celková hodnota balíků je {total_value},- Kč.')
+assert hasattr(zamestnanec, 'jmeno')
+assert hasattr(zamestnanec, 'rocni_plat')
+assert hasattr(zamestnanec, 'pozice')
+assert isinstance(zamestnanec.jmeno, str)
+assert isinstance(zamestnanec.rocni_plat, int)
+assert isinstance(zamestnanec.pozice, str)
+    
+print(ODDELOVAC)
 
-# Vytvoř si proměnnou total_value, do které si s využitím cyklu budeš ukládat celkovou hodnotu všech balíků. Na začátku bude mít hodnotu 0.
-# Vytvoř cyklus, který projde seznam package_list.
-# Vyber funkci, která je podle tebe nejvhodnější pro zajištění bezpečného čtení atributu value. Můžeš použít funkci isinstance(), hasattr() i getattr(). Přičti hodnotu balíku k proměnné total_value, aniž by program skončil chybou u objektu package_2.
-# Na konci programu vypiš, jaká je celková hodnota balíků v autě.
+# ÚKOL Č. 3 - ŘEDITEL:
 
-# TODO
+class Reditel(Zamestnanec):
+    def __init__(self, jmeno, rocni_plat, pozice = 'Ředitel', oblibene_zvire = Zvire):
+        super().__init__(jmeno, rocni_plat, pozice)
+        self.oblibene_zvire = oblibene_zvire
 
-# 2. Celková hodnota balíků podruhé
-# Vedení společnosti si uvědomilo, že do hodnoty balíků v autě by se neměly započítávat balíky,
-# které už byly doručeny, protože již byly předány příjemci a nebudou tedy ukradeny nebo zničeny.
+    def __str__(self):
+        return f'{self.jmeno} je na pozici {self.pozice} a roční plat má {self.rocni_plat},- Kč. Jeho oblíbené zvíře je {self.oblibene_zvire.druh} {self.oblibene_zvire.jmeno}.'
+      
+reditel = Reditel('Karel', 800000, oblibene_zvire = zvire)
 
-# Uprav kód, který vytváří balíky, aby byl jeden balík vytvořený ve stavu doručen.
-# Uprav cyklus, aby započítal hodnotu pouze těch balíků, které jsou ve stavu nedoručen.
-# Je třeba pro čtení použít některou z funkcí isinstance(), hasattr() nebo getattr().
+print(reditel)
 
-package_1.deliver()
+assert reditel.pozice == 'Ředitel'
+assert isinstance(reditel.oblibene_zvire, Zvire)
 
-total_value = 0
+'''print(ODDELOVAC)
 
-for one in package_list:
-    if hasattr(one, 'value') and one.state == "nedoručen":
-            total_value += one.value
-            print(f'Celková hodnota balíků je {total_value},- Kč.')
+# ÚKOL Č. 4 - ZOO:
+class Zoo:
+    def __init__(self, jmeno: str, adresa: str, reditel: Reditel, zamestnanci: list[Zamestnanec], zvirata: list[Zvire]):
+        self.jmeno = jmeno
+        self.adresa = adresa
+        self.reditel = reditel
+        self.zamestnanci = zamestnanci
+        self.zvirata = zvirata
 
-# TODO
+    def __str__(self):
+        return f'Zoo {self.jmeno} se nachází na adrese {self.adresa}. V této ZOO pracuje {self.zamestnanci} zaměstnanců a chová se zde {self.zvirata} zvířat.'
 
-# 3. Vypravěči
-# Nyní se vrátíme k práci pro nakladatelství, pro které jsme již připravili třídy Item, Book a AudioBook.
-# V e-shopech se často objevuje možnost filtrování produktů. Uvažujme například, že někteří fanoušci a fanynky audioknih
-# vybírají knihy podle herce a herečky, kteří je čtou. Preferované jméno bude uložené v proměnné favourite_narrator.
-# Napiš kód, který projde všechny položky v seznamu all_items a vypíše názvy (atribut title) objektů,
-# pro které se hodnota atributu narrator rovná hodnota v proměnné favourite_narrator.
-# Pozor na to, že v seznamu all_items jsou i běžné knihy, které vypravěče nemají.
-# Zajisti, aby program neskončil chybou, protože některý objekt nemá atribut narrator.
+    def vaha_vsech_zvirat_v_zoo(self):
+        celkova_vaha = 0
+        for zvire in self.zvirata:
+            celkova_vaha += zvire.vaha
+        return celkova_vaha
 
-class Item:
-    def __init__(self, title, price):
-        self.title = title
-        self.price = price
+    def mesicni_naklady_na_zamestnance(self):
+        vsichni_zamestnanci = [self.reditel] + self.zamestnanci[:]
+        celkove_naklady = 0
+        for zamestnanec in vsichni_zamestnanci:
+            celkove_naklady += zamestnanec.rocni_plat
+        return celkove_naklady / 12
+    
+    def mesicni_naklady_na_zamestnance_2(self):
+        vsichni_zamestnanci = [tereza, aneta, martin, reditel]
+        celkove_naklady = 0
+        for zamestnanec in vsichni_zamestnanci:
+            celkove_naklady += zamestnanec.rocni_plat
+        return celkove_naklady / 12
 
-    def get_info(self):
-        return f"{self.title} stojí {self.price} Kč."
+zoo = Zoo('ZOO Praha', 'U Trojského zámku 3/120', reditel, zamestnanci, zvirata)
 
-    def get_time_to_read(self):
-        pass
 
-# Navic pages atribut
-class Book(Item):
-    def __init__(self, title, price, pages):
-        super().__init__(title, price)
-        self.pages = pages
-
-    def get_info(self):
-        return f"Kniha {self.title} má {self.pages} stran a stojí {self.price} Kč."
-
-    def get_time_to_read(self, minutes=4):
-        return self.pages * minutes / 60
-
-# navic duration_in_hours a narrator
-class AudioBook(Item):
-    def __init__(self, title, price, duration_in_hours, narrator):
-        super().__init__(title, price)
-        self.duration_in_hours = duration_in_hours
-        self.narrator = narrator
-
-    def get_info(self):
-        # Jen ukazka jak se treba popasovat s vypisem
-        return super().get_info().replace('Kniha', 'Audiokniha')
-
-    def get_time_to_read(self):
-        return self.duration_in_hours
-
-favourite_narrator = "Zbyšek Horák"
-item_1 = AudioBook("Problém tří těles", 299, 14.4, "Zbyšek Horák")
-item_2 = Book("Kadet Hornblower", 399, 242)
-item_3 = AudioBook("Odysseus", 389, 13.7, "Lukáš Hlavica")
-
-all_items = [item_1, item_2, item_3]
-
-'''for one in all_items:
-    if hasattr (one, 'narrator') and one.narrator == favourite_narrator:
-        print(one.title)'''
-
-# TODO
-
-###### PROVAZANI OBJEKTU ######
-
-# 1. Auta a balíky
-# U našich balíků budeme nově evidovat, který řidič(ka) balík doručuje.
-# Díky tomu pak bude možné odeslat SMS zprávu s číslem řidiče (řidičky), aby mohli zákazníci v případě potřeby řidiče (řidičku) kontaktovat.
-
-# Vytvoř třídu Driver, která bude mít atributy name a phone_number.
-# Dále uprav třídu Package. Třída bude mít nově atribut driver, ve kterém bude uložen(a) řidič(ka) doručující balík.
-# Uprav i třídu ValuablePackage, aby v metodě __init__() předala hodnotu parametru driver metodě __init__ rodičovské třídy.
-# Přidej třídě Package metodu send_message(), která odešle zprávu s textem:
-# "Dnes budeme doručovat váš balík. V případě potřeby kontaktujte řidiče na čísle: " Na konec zprávy doplň telefonní číslo.
-
-# TODO
+print(zoo.reditel)
+print('Celková váha zvířat v ZOO:', zoo.vaha_vsech_zvirat_v_zoo())
+print('Měsíční náklady na zaměstnance:', zoo.mesicni_naklady_na_zamestnance())
+print('Měsíční náklady na zaměstnance:', zoo.mesicni_naklady_na_zamestnance_2())'''
